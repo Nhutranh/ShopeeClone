358943114c45c57a91d5ca7517daab277826c9ee
NhuTranh <nhutranh008@gmail.com>
2024-07-15 17:00:12 +0700

    UI header

--- shopee/src/useRouteElement.tsx
+++ shopee/src/useRouteElement.tsx
@@ -3,12 +3,17 @@ import ProductList from './pages/ProductList'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Registerlayout from './layouts/RegisterLayout'
+import MainLayout from './layouts/RegisterLayout/MainLayout'
 
 export default function useRouteElement() {
   const routeElement = useRoutes([
     {
       path: '/',
-      element: <ProductList />
+      element: (
+        <MainLayout>
+          <ProductList />
+        </MainLayout>
+      )
     },
     {
       path: '/login',
